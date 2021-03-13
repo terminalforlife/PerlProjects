7ee3a6b123e45d07db9a5162747e65cfc5f033e3
terminalforlife <terminalforlife@yahoo.com>
2021-03-13 23:51:37 +0000

    Optimize and clean up APT History's module

--- source/apt-history/APTHistory.pm
+++ source/apt-history/APTHistory.pm
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Project Name      - PerlProjects/source/apt-history/APTHistory.pm
 # Started On        - Mon 18 Jan 02:48:06 GMT 2021
-# Last Change       - Mon 18 Jan 03:22:39 GMT 2021
+# Last Change       - Sat 13 Mar 23:50:52 GMT 2021
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
@@ -15,7 +15,7 @@ use strict;
 use warnings;
 use autodie;
 
-my $CurVer = '2021-01-18';
+my $CurVer = '2021-03-13';
 
 sub new {
 	my $Class = $_[0];
@@ -30,27 +30,27 @@ sub new {
 	return($Self)
 }
 
-sub version {
-	return("$CurVer\n")
-}
+sub version {"$CurVer\n"}
 
 # I could get something like this done via the APT perl module, but that's yet
 # another dependency, so I thought I'd just go the pure-Perl route.
 sub packages { # Usage: packages(FILE)
-	-f $_[1] or die("File '$_[1]' not found");
-	-r $_[1] or die("File '$_[1]' unreadable");
+	my $File = $_[1];
+
+	-f $File or die("File '$File' not found");
+	-r $File or die("File '$File' unreadable");
 
 	my (@InstalledPKGs, $InstalledPKG, $InstalledArch, $PKGStatus);
-	if (open(my $FH, '<', $_[1])) {
+	if (open(my $FH, '<', $File)) {
 		while (<$FH>) {
 			chomp();
 
-			if (/^Package:/) {
-				$InstalledPKG = (split(' '))[1]
-			} elsif (/^Status:/) {
-				$PKGStatus = $_ =~ s/^Status: //r
-			} elsif (/^Architecture:/) {
-				$InstalledArch = (split(' '))[1];
+			if (/^Package: (.*)$/) {
+				$InstalledPKG = $1;
+			} elsif (/^Status: (.*)$/) {
+				$PKGStatus = $1
+			} elsif (/^Architecture: (.*)$/) {
+				$InstalledArch = $1;
 				$InstalledPKG = "$InstalledPKG:$InstalledArch";
 
 				if ($PKGStatus eq 'install ok installed') {
@@ -78,15 +78,14 @@ sub history { # Usage: history(FILE)
 		while (<$FH>) {
 			chomp();
 
-			if (/^Start-Date:/) {
-				($Date, $Time) = (split(' '))[1..2]
-			} elsif (/^Commandline:/) {
-				$Command = (m{(?:$& )(.*)})[0]
-			} elsif (/^Requested-By:/) {
-				my $ReqBy = (m{(?:$&)(.*)})[0];
-				($ReqByUser, $ReqByUID) = (split(' ', $ReqBy))[0..1];
-			} elsif (/^(Install|Purge|Remove|Upgrade):/) {
-				($Action, $Packages) = (m{(^$&)(.*)})[0..1];
+			if (/^Start-Date: (.*)\s+(.*)$/) {
+				($Date, $Time) = ($1, $2)
+			} elsif (/^Commandline: (.*)$/) {
+				$Command = $1
+			} elsif (/^Requested-By: (.*) \((\d+)\)$/a) {
+				($ReqByUser, $ReqByUID) = ($1, $2)
+			} elsif (/^(Install|Purge|Remove|Upgrade): (.*)$/) {
+				($Action, $Packages) = ($1, $2);
 
 				# We're finished gathering data, so populate hash, now.
 				$History{"${Date}_$Time"} = {
@@ -107,4 +106,4 @@ sub history { # Usage: history(FILE)
 	return(%History)
 }
 
-return(1)
+1
